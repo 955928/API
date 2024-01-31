8a2b1e8ebbd3ae3f3441d41ac336c8d51d2d2c46
955928 <d.chathuant@hotmail.com>
2024-01-31 15:08:43 +0100

    done

--- Exercices/Streamlit/Iris_Dashboard.py
+++ Exercices/Streamlit/Iris_Dashboard.py
@@ -5,7 +5,7 @@ import pandas as pd
 iris_df = pd.read_csv('iris_data.csv')
 
 st.set_page_config(page_title='Iris Dataset')
-st.header('Iris Machine Learnign Project')
+st.header('Iris Machine Learning Project')
 st.markdown('Deployment of the Iris Dataset machine learning model using XGBoost')
 st.markdown('Use this dashboard to understand the data and to make predictions')
 st.markdown('')
